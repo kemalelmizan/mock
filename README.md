7c0fe6ddb00d2083cf091deb6e0b580b9677b31b
Kemal Elmizan <kemalelmizan@gmail.com>
2021-01-02 06:52:35 +0700

    Update log on response and add desc on readme

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # mock
 
+No-dependency api server in your local machine with 1 file route configuration.
+Live-reloads the mock server if API config changes.
+
 ## Prerequisite
 [Deno 1.6.x](https://deno.land/#installation)
 
 ## Installation
 ```
 git clone https://github.com/kemalelmizan/mock.git
-echo "alias mock=\"cd $(pwd)/mock && deno run --allow-read --allow-run ./wrapper.ts\"" >> ~/.bashrc # or ~/.zshrc
+echo "alias mock=\"cd $(pwd)/mock && deno run --allow-read --allow-run ./wrapper.ts || cd -\"" >> ~/.bashrc # or ~/.zshrc
 source ~/.bashrc # or ~/.zshrc
 ```
 
--- server.ts
+++ server.ts
@@ -8,12 +8,12 @@ const server = serve({ hostname: api.hostname, port: api.port });
 console.log(api, args);
 
 for await (const request of server) {
-  console.log(request.url, request.method);
   const route = api.routes.filter((route: {
     url: string;
     method: string;
   }) => route.method === request.method && route.url === request.url)
   const response = route.length === 0 ? api?.not_found : route?.[0]?.response;
   response.body = typeof response.body === "string" ? response.body : JSON.stringify(response.body)
+  console.log(`${new Date()}`, request.method, request.url, '>', response);
   request.respond({ ...response })
 }
