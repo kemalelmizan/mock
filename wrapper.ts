43ecb21044865518880453d92ac811ff69c70a5a
kemalelmizan <kemalelmizan@gmail.com>
2021-01-03 08:01:25 +0700

    Run vscode on -c

--- wrapper.ts
+++ wrapper.ts
@@ -6,10 +6,9 @@ const cmd = ["deno", "run", "--allow-net", "--allow-read", "server.ts", ...args]
 const carg = parse(args)?.["c"]
 
 if (carg && typeof carg !== "string") {
-    const api = `${cwd()}\\api.ts`
+    const api = `${cwd()}/api.ts`
     console.log(`Opening ${api}`)
     try {
-        run({ cmd: ["${EDITOR:-${VISUAL:-code}}", api] })
         run({ cmd: ["code", api] })
     } catch (error) {
         console.error("Some error occured: ", error)
